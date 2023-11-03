7ab0468c62bb2003420285145fc6fdcbd5cd36b2
m.sboskabadi <m.sboskabadi@gmail.com>
2023-11-03 16:05:04 +0330

    init commit

---
+++ EfIntro/Model/Person.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfIntro.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database= EfIntroDb; User Id=sa; Password=0910173532; Encrypt=False");
+        }
+    }
+    
+}
---
+++ EfIntro/Model/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfIntro.Model
+{
+    public class Repository
+    {
+        public void AddPerson(string FirstName, string LastName)
+        {
+            EfDbContext db = new EfDbContext();
+
+            Person person = new()
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+
+            db.Persons.Add(person);
+            db.SaveChanges();
+        }
+
+        public void UpdatePerson(int Id, string firstName, string lastName)
+        {
+            EfDbContext db = new EfDbContext();
+            Person person = db.Persons.Find(Id); 
+            
+            if(person != null)
+            {
+                person.FirstName = firstName;
+                person.LastName = lastName;
+                db.SaveChanges();
+            }
+        }
+
+        public void PrintPeople()
+        {
+            EfDbContext db = new EfDbContext();
+            var persons = db.Persons.ToList();
+
+            foreach(var person in persons)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+        }
+
+        public void DeletePerson(int Id)
+        {
+            EfDbContext db = new EfDbContext();
+            Person person = db.Persons.Find(Id);
+            if(person != null)
+            {
+                db.Persons.Remove(person);
+                db.SaveChanges();
+            }
+        }
+    }
+}
